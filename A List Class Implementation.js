9f3f827d04dbe90838b8ddc6a5ae7809497dc4c3
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:27:46 +0430

    Add files via upload

---
+++ A List Class Implementation.js
@@ -0,0 +1,21 @@
+function List() {
+ this.listSize = 0;
+ this.pos = 0;
+ this.dataStore = []; // initializes an empty array to store list elements
+ this.clear = clear;
+ this.find = find;
+ this.toString = toString;
+ this.insert = insert;
+ this.append = append;
+ this.remove = remove;
+ this.front = front;
+ this.end = end;
+ this.prev = prev;
+ this.next = next;
+ this.length = length;
+ this.currPos = currPos;
+ this.moveTo = moveTo;
+ this.getElement = getElement;
+ this.length = length;
+ this.contains = contains;
+ }
