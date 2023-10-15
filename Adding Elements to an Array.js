a8bc6065e69ac0bf2f877dd1aad12b7e4080ba1e
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:28:54 +0430

    Add files via upload

---
+++ Adding Elements to an Array.js
@@ -0,0 +1,9 @@
+var nums = [1,2,3,4,5];
+print(nums); // 1,2,3,4,5
+nums.push(6);
+print(nums); // 1,2,3,4,5,6
+
+var nums = [1,2,3,4,5];
+print(nums); // 1,2,3,4,5
+nums[nums.length] = 6;
+print(nums); // 1,2,3,4,5,6
