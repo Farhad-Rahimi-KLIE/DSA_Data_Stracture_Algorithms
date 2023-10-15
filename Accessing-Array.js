af4f6f7d14a7a862cd63845a8fb0d136beaa8e0b
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:28:19 +0430

    Add files via upload

---
+++ Accessing-Array.js
@@ -0,0 +1,14 @@
+var nums = [];
+for (var i = 0; i < 100; ++i) {
+ nums[i] = i+1;
+}
+var numbers = [1,2,3,4,5];
+var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] +
+ numbers[4];
+print(sum); // displays 15
+var numbers = [1,2,3,5,8,13,21];
+var sum = 0;
+for (var i = 0; i < numbers.length; ++i) {
+ sum += numbers[i];
+}
+print(sum); // displays 53
