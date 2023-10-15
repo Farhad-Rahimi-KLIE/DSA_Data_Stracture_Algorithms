1a6a939dd36ec752fd4d5d85a8d77fe1bf4d0624
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:29:47 +0430

    Add files via upload

---
+++ Adding Sorting to the Dictionary Class.js
@@ -0,0 +1,18 @@
+load("Dictionary.js");
+var pbook = new Dictionary();
+pbook.add("Raymond","123");
+pbook.add("David", "345");
+pbook.add("Cynthia", "456");
+pbook.add("Mike", "723");
+pbook.add("Jennifer", "987");
+pbook.add("Danny", "012");
+pbook.add("Jonathan", "666");
+pbook.showAll();
+Here is the output of the program:
+Cynthia -> 456
+Danny -> 012
+David -> 345
+Jennifer -> 987
+Jonathan -> 666
+Mike -> 723
+Raymond -> 123
