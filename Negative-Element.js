ebbee7fc97f5373e0b4424de761b1bd694b78e92
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:55:47 +0430

    Add files via upload

---
+++ Negative-Element.js
@@ -0,0 +1,12 @@
+function Negative(data){
+ let are = [];
+ for(let i=0; i<data.length; i++){
+  if(data[i]>i){
+   arr.push(data[i])
+  }
+ }
+ return arr;
+}
+let data = [1,6,5,3,2,-7,-4,3,-3];
+let result = Negative(data);
+console.log(result)
