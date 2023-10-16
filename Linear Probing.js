3c47a4091c0f51de1852beced401e6b29cd06f91
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:52:21 +0430

    Add files via upload

---
+++ Linear Probing.js
@@ -0,0 +1,14 @@
+function put(key, data) {
+ var pos = this.betterHash(key);
+ if (this.table[pos] == undefined) {
+ this.table[pos] = key;
+ this.values[pos] = data;
+ }
+ else {
+ while (this.table[pos] != undefined) {
+ pos++;
+ }
+ this.table[pos] = key;
+ this.values[pos] = data;
+ }
+}
