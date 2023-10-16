11f8e84ca342dd72cc7288e2fd97e22bac91e8e0
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:32:33 +0430

    Add files via upload

---
+++ Array-Right-Rotate.js
@@ -0,0 +1,9 @@
+function Rotate(){
+ let first = data.shift();
+ let second = data.shift();
+ let third = data.shift();
+ data.push(first,second,third)
+ return data
+}
+let data = [1,2,3,4,5,6,7,8];
+console.log(Rotate(data))
