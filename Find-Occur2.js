ce7fa2922a60351ea600ad404b18f7a35f044503
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:45:46 +0430

    Add files via upload

---
+++ Find-Occur2.js
@@ -0,0 +1,13 @@
+function Occur(data,n,x){
+ let res = 0;
+ let data1 = [];
+ for(let i=0; i<n; i++){
+  if(x==data[i]){
+   data1.push(res++)
+  }
+ return data1
+}
+let data = [1,2,2,2,2,3,4,5,6,6,6,7,8];
+let n = data.length;
+let x = 6;
+console.log(Occur(data,n,x))
