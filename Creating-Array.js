cbed6bdeadbe1b6526af5d172a9e516663d4e0c6
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:40:27 +0430

    Add files via upload

---
+++ Creating-Array.js
@@ -0,0 +1,6 @@
+var numbers = [];
+print(numbers.length); // displays 0
+var numbers = [1,2,3,4,5];
+print(numbers.length); // displays 5
+var numbers = new Array();
+print(numbers.length); // displays 0
