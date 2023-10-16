e662d8138e406c7d49a8b281b658066117995e34
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:45:11 +0430

    Add files via upload

---
+++ Find-minimum-element.js
@@ -0,0 +1,12 @@
+function FindMinimum(data,n){
+ let min = data[0];
+ for(let i=0; i<n; i++){
+  if(data[i]<min){
+   min = data[i];
+  }
+ }
+ return min
+}
+let data = [5,6,1,2,3,4];
+let n = data.length;
+console.log(FindMinimum(data,n))
