f210a3a8d8bc5376c2d9f509a550cbf654f7181f
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:59:12 +0430

    Add files via upload

---
+++ Putting Array Elements in Order.js
@@ -0,0 +1,7 @@
+var nums = [1,2,3,4,5];
+nums.reverse();
+print(nums); // 5,4,3,2,1
+
+var names = ["David","Mike","Cynthia","Clayton","Bryan","Raymond"];
+nums.sort();
+print(nums); // Bryan,Clayton,Cynthia,David,Mike,Raymond
