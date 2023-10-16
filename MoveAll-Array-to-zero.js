4eb4fdd7ed7092bb3f18a79228e936276b7c9e4b
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:53:51 +0430

    Add files via upload

---
+++ MoveAll-Array-to-zero.js
@@ -0,0 +1,11 @@
+let data = [1,20,30,0,8,7,59,65,0,88];
+for(let i=0; i<data.length; i++){
+ for(let j=0; j<data.length; j++){
+  if(data[j]>data[j+1]){
+   let temp = data[j];
+   data[j]=data[j+1];
+   data[j+1]=temp;
+  }
+ }
+}
+console.log(data)
