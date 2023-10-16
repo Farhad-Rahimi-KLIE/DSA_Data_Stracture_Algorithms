311636eb952ac0d5d296815a91e45d6037870002
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:00:44 +0430

    Add files via upload

---
+++ Removing Elements from an Array.js
@@ -0,0 +1,10 @@
+var nums = [1,2,3,4,5,9];
+nums.pop();
+print(nums); // 1,2,3,4,5
+
+var nums = [9,1,2,3,4,5];
+print(nums);
+for (var i = 0; i < nums.length; ++i) {
+ nums[i] = nums[i+1];
+}
+print(nums); // 1,2,3,4,5,
