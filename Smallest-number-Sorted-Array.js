f8756a7476b3bb93781f0c16bd8f60d750e4eeaf
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:06:47 +0430

    Add files via upload

---
+++ Smallest-number-Sorted-Array.js
@@ -0,0 +1,19 @@
+function Positive(data,n){
+ let min = data[0];
+ for(let i=0; i<n; i++){
+  for(let j=0; j<n; j++){
+   if(data[j]<data[j+1]){
+    let temp = data[j];
+    data[j]=data[j+1];
+    data[j+1]=temp;
+    if(data[j]<min){
+     min = data[i]
+    }
+   }
+  } 
+ }
+ return min
+}
+let data = [2,3,6,8,7,-1,-10,15];
+let n = data.length;
+console.log(Positive(data,n))
