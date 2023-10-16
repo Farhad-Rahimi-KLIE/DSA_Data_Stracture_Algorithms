1bd9137866fed5162da5e9861d6d96578e4f85f8
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:10:23 +0430

    Add files via upload

---
+++ Storing and Retrieving Data in a Hash Table.js
@@ -0,0 +1,19 @@
+load("Hashing.js");
+var pnumbers = new HashTable();
+var name, number;
+for (var i = 0; i < 3; i++) {
+ putstr("Enter a name (space to quit): ");
+ name = readline();
+ putstr("Enter a number: ");
+ number = readline();
+}
+name = "";
+putstr("Name for number (Enter quit to stop): ");
+while (name != "quit") {
+ name = readline();
+ if (name == "quit") {
+ break;
+ }
+ print(name + "'s number is " + pnumbers.get(name));
+ putstr("Name for number (Enter quit to stop): ");
+}
