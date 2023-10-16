150ba889116e883dcc87beaff121faebf619e016
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:38:59 +0430

    Add files via upload

---
+++ Creating Arrays from Strings.js
@@ -0,0 +1,16 @@
+var sentence = "the quick brown fox jumped over the lazy dog";
+var words = sentence.split(" ");
+for (var i = 0; i < words.length; ++i) {
+ print("word " + i + ": " + words[i]);
+}
+
+word 0: the
+word 1: quick
+word 2: brown
+word 3: fox
+word 4: jumped
+word 5: over
+word 6: the
+word 7: lazy
+word 8: dog
+Aggregate Arra
