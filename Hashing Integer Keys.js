f5fd5b2a56d89dda778c306ce857742305c5944d
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:48:41 +0430

    Add files via upload

---
+++ Hashing Integer Keys.js
@@ -0,0 +1,13 @@
+function getRandomInt (min, max) {
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function genStuData(arr) {
+ for (var i = 0; i < arr.length; ++i) {
+ var num = "";
+ for (var j = 1; j <= 9; ++j) {
+ num += Math.floor(Math.random() * 10);
+ }
+ num += getRandomInt(50, 100);
+ arr[i] = num;
+ }
+}
