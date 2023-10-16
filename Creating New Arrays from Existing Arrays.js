b33b1faced158de3d97c45310e2f93aa2994a00c
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:39:52 +0430

    Add files via upload

---
+++ Creating New Arrays from Existing Arrays.js
@@ -0,0 +1,6 @@
+var cisDept = ["Mike", "Clayton", "Terrill", "Danny", "Jennifer"];
+var dmpDept = ["Raymond", "Cynthia", "Bryan"];
+var itDiv = cis.concat(dmp);
+print(itDiv);
+itDiv = dmp.concat(cisDept);
+print(itDiv);
