2e1d474fea09302c4b680284b9cdf13a5b494e9a
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:02:09 +0430

    Add files via upload

---
+++ Reverse-Array.js
@@ -0,0 +1,11 @@
+let data = [1,2,3,4,5,6,7,8,9];
+for(let i=0; i<data.length; i++){
+ for(let j=0; j<data.length; j++){
+  if(data[j]<data[j+1]){
+   let temp = data[j];
+   data[j]=data[j+1];
+   data[j+1]=temp
+  }
+ }
+}
+console.log(data)
