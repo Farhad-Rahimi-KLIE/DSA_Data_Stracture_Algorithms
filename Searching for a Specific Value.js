20c6bcb2545769ef3ac4c537099600ccc081f4b4
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:03:19 +0430

    Add files via upload

---
+++ Searching for a Specific Value.js
@@ -0,0 +1,15 @@
+function find(data) {
+ var current = this.root;
+ while (current.data != data) {
+ if (data < current.data) {
+ current = current.left;
+ }
+ else {
+ current = current.right;
+ }
+ if (current == null) {
+ return null;
+ }
+ }
+ return current;
+}
