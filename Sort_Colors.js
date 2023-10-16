3b127db7d3720ef6e4fd91036771b8342f3e54b5
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:07:28 +0430

    Add files via upload

---
+++ Sort_Colors.js
@@ -0,0 +1,15 @@
+function Sort_Colors(data){
+ for(let i=0; i<n; i++){ 
+  for(let j=0; j<n; j++){
+   if(data[j]>data[j+1]){
+    let temp = data[j];
+    data[j] = data[j+1];
+    data[j+1]=temp;
+   }
+  }
+ }
+ return data;
+}
+let data = [2,0,2,1,1,0];
+let n = data.length;
+console.log(Sort_Colors(data))
