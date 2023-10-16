43368f3f4ac2e0c20e93e59a4b647258e8547510
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:36:36 +0430

    Add files via upload

---
+++ Circularly Linked Lists.js
@@ -0,0 +1,10 @@
+function LList() {
+ this.head = new Node("head");
+ this.head.next = this.head;
+ this.find = find;
+ 
+ this.insert = insert;
+ this.display = display;
+ this.findPrevious = findPrevious;
+ this.remove = remove;
+ }
