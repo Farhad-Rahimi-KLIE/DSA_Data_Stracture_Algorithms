3a5936793fceb489abedbe35898e4f626f8bea9f
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:30:38 +0430

    Add files via upload

---
+++ Algorithm and pseudocode for the Quicksort algorithm.js
@@ -0,0 +1,16 @@
+function qSort(list) {
+ if (list.length == 0) {
+ return [];
+ }
+ var lesser = [];
+ var greater = [];
+ var pivot = list[0];
+ for (var i = 1; i < list.length; i++) {
+ if (list[i] < pivot) {
+ lesser.push(list[i]);
+ } else {
+ greater.push(list[i]);
+ }
+ }
+ return qSort(lesser).concat(pivot, qSort(greater));
+ }
