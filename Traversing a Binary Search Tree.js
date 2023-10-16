eb6c9da937affafa03d0647eadc67bd7b8214599
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:17:01 +0430

    Add files via upload

---
+++ Traversing a Binary Search Tree.js
@@ -0,0 +1,17 @@
+function inOrder(node) {
+ if (!(node == null)) {
+ inOrder(node.left);
+ putstr(node.show() + " ");
+ inOrder(node.right);
+ }
+}
+var nums = new BST();
+nums.insert(23);
+nums.insert(45);
+nums.insert(16);
+nums.insert(37);
+nums.insert(3);
+nums.insert(99);
+nums.insert(22);
+print("Inorder traversal: ");
+inOrder(nums.root);
