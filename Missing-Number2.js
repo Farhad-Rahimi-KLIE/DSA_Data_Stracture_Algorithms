fab0e3bec744a219d476b3b1bf72c073814475ce
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:52:38 +0430

    Add files via upload

---
+++ Missing-Number2.js
@@ -0,0 +1,11 @@
+function FindMissing(data,n){
+let total = Math.floor((n+1)*(n+2)/2);
+for(let i=0; i<n; i++){
+ total-=data[i]
+}
+return total;
+}
+let data = [1,2,4,6,3,7,8];
+let n = data.length;
+let result = FindMissing(data,n);
+console.log(result)
