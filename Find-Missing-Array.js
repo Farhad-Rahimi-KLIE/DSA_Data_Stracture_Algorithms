8946070c40f212f56f691c9c9f87162d7f982d69
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:44:54 +0430

    Add files via upload

---
+++ Find-Missing-Array.js
@@ -0,0 +1,13 @@
+let data = [1,2,3,4,5,67,8];
+let position = 4;
+let missing = 5;
+for(let i=data.length-1; i>0; i--){
+ if(i>=position){
+  data[i+1]=data[i]
+  if(data[i]==position){
+   data[i]=missing;
+   data.length = data.length-1;
+  }
+ }
+}
+console.log(data)
