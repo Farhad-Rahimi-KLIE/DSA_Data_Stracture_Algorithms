b59da330706d72c39fa0f46c935debd2e6570a63
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:37:58 +0430

    Add files via upload

---
+++ Concatenation-Array.js
@@ -0,0 +1,9 @@
+function Concatenation(data){
+ let data2 = [];
+ for(let i=0; i<data.length; i++){
+  data2.push(data[i]);
+ }
+ return data2.concat(1,2,1)
+}
+let data = [1,2,1];
+console.log(Concatenation(data))
