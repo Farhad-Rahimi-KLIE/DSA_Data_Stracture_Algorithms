2a1c03682d180e530b9ddfe84616021fbd6b7d43
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:00:07 +0430

    Add files via upload

---
+++ Remove-Element (۱).js
@@ -0,0 +1,13 @@
+function Remove(data){
+ let best = 3;
+ for(let i=0; i<n; i++){
+  if(data[i]==best){
+   data[i]=data[i+1];
+  }
+ }
+ data.length = data.length-2;
+ return data;
+}
+let data = [3,2,2,3];
+let n = data.length;
+console.log(Remove(data))
