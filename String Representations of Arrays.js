ad206f93804026c33dbca73b5b2264aff4ebd9a2
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:10:01 +0430

    Add files via upload

---
+++ String Representations of Arrays.js
@@ -0,0 +1,5 @@
+var names = ["David", "Cynthia", "Raymond", "Clayton", "Mike", "Jennifer"];
+var namestr = names.join();
+print(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
+namestr = names.toString();
+print(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
