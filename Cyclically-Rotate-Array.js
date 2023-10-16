a36d1077715382085ade338cbf5bea3b45b02c5b
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:40:10 +0430

    Add files via upload

---
+++ Cyclically-Rotate-Array.js
@@ -0,0 +1,9 @@
+let data = [1,2,3,4,5];
+let newEl = 5;
+let start = 0;
+for(let i=4; i<data.length; i++){
+ data[i]=data[i+1];
+ data.length = data.length-1;
+ data.unshift(5)
+ console.log(data)
+}
