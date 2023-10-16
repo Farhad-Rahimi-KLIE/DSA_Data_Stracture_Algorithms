d9075d78bd23b19a1382bbe22b901a05afca0076
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 09:46:26 +0430

    Update String Representations of Arrays.js

--- String Representations of Arrays.js
+++ String Representations of Arrays.js
@@ -1,3 +1,4 @@
+// javascript algorithms 
 var names = ["David", "Cynthia", "Raymond", "Clayton", "Mike", "Jennifer"];
 var namestr = names.join();
 print(namestr); // David,Cynthia,Raymond,Clayton,Mike,Jennifer
