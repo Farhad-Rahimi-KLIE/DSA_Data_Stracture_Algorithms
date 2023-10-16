0ceb65c0c362b33e270c0765b8914099353937eb
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:51:50 +0430

    Add files via upload

---
+++ Largest-value.js
@@ -0,0 +1,9 @@
+let value = [2,7,11,15];
+let target = 9;
+for(let i=0; i<value.length; i++){
+ for(let j=1; j<value.length; j++){
+  if(value[i] + valie[j]===target){
+   console.log(value[i],value[j])
+  }
+ }
+}
