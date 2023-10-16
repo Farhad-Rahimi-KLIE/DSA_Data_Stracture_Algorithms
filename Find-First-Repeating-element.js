81ec3c0c584c0bf0226e0c7e78fd400a9c1c4094
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:43:13 +0430

    Add files via upload

---
+++ Find-First-Repeating-element.js
@@ -0,0 +1,13 @@
+function Repeating(data){
+ let find = 5;
+ let arr = [];
+ for(let i=0; i<data.length; i++){
+  if(data[i]==find){
+   arr.push(data[i])
+  }
+ }
+ return arr;
+}
+let data = [10,5,3,4,5,77,10,6];
+let result = Repeating(data);
+console.log(result)
