d974f8fd83a52c1976e1381491ec6197e9ea8674
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 04:55:15 +0430

    Add files via upload

---
+++ Multiple Base Conversions.js
@@ -0,0 +1,12 @@
+function mulBase(num, base) {
+ var s = new Stack();
+ do {
+ s.push(num % base);
+ num = Math.floor(num /= base);
+ } while (num > 0);
+ var converted = "";
+ while (s.length() > 0) {
+ converted += s.pop();
+ }
+ return converted;
+ }
