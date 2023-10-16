fae593b76d3967ef5d6affdebcffb3ecba9295f0
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-16 05:09:42 +0430

    Add files via upload

---
+++ Sorted-Array-to-Balanced-BST.js
@@ -0,0 +1,18 @@
+function BalancedBST(data,n){
+ let data2 = [];
+ for(let i=0; i<n; i++){
+ data2.push(data[i])
+ }
+ for(let i=0; i<n; i++){
+ if(data[i]<3){
+  data2[i]=data[i]
+ }
+ if(data[i]>3){
+  data2[i]=data[i]
+ }
+ }
+ return data2;
+ }
+ let data = [1,2,3,4];
+ let n = data.length;
+ console.log(BalancedBST(data,n))
