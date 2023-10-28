57252e76c8e863828eef404a7c77c480f9b3150b
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-28 05:15:19 +0430

    Add files via upload

---
+++ challenge_twelve.js
@@ -0,0 +1,12 @@
+function findFibonacci(n){
+ if(n==0){
+  return 0;
+  if(n==1){
+   return 1;
+   
+   return findFibonacci(n-1) + findFibonacci(n-2);
+  }
+ } 
+}
+let n = findFibonacci(10);
+console.log(n)
