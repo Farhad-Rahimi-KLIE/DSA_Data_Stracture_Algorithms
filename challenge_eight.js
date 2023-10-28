afa4e8a9cc07dcda60ae37f53afe9fb41538e3e3
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-28 05:11:57 +0430

    Add files via upload

---
+++ challenge_eight.js
@@ -0,0 +1,13 @@
+function getPositive(arr){
+ let arr2 = [];
+ for(let i=0; i<arr.length; i++){
+  let el = arr[i];
+  if(el<=i){
+   arr2.push(el);
+  } 
+ }
+ return arr2
+}
+let arr = [-5,5,4,4,-7,-3,6,7];
+let ar = getPositive(arr);
+console.log(ar)
