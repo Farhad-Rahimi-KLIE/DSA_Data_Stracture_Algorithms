7d29c7bba25d9b33a46a30bfcae630ecb5b17075
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2023-12-02 01:00:39 +0600

    Update A Dynamic Programming Example.js

--- A Dynamic Programming Example.js
+++ A Dynamic Programming Example.js
@@ -1,9 +1,25 @@
-// javascript Algorithms
-function recurFib(n) {
- if (n < 2) {
- return n;
- }
- else {
- return recurFib(n-1) + recurFib(n-2);
- }
+// JavaScript function to calculate Fibonacci sequence using recursion and memoization
+
+// Initialize an object to store already calculated values
+const memo = {};
+
+function improvedFib(n) {
+    // If the value is already calculated, return it from memo
+    if (memo[n] !== undefined) {
+        return memo[n];
+    }
+
+    // Base cases: If n is 0 or 1, return n
+    if (n < 2) {
+        memo[n] = n;
+        return n;
+    }
+
+    // Recursive case: Calculate Fibonacci for n using memoization
+    memo[n] = improvedFib(n - 1) + improvedFib(n - 2);
+    return memo[n];
 }
+
+// Example usage:
+const result = improvedFib(10); // Change the argument to calculate Fibonacci for a different number
+console.log(result); // Output the result
